e0bd7e4cd0d139ca4bc98b293601354a6919aa0f
SaanviSinha <69468899+SaanviSinha@users.noreply.github.com>
2023-11-27 20:04:07 -0500

    Add files via upload

---
+++ virus_auto.py
@@ -0,0 +1,6 @@
+with open("file.txt", 'w+') as k:
+    k.write("""A computer file that contains an encoded sequence of instructions that the system can 
+            execute directly when the user clicks the file icon. 
+            Executable files commonly have an .exe file extension, 
+            but there are hundreds of other executable file formats.
+            So, every true .exe file is an executable but not every executable file has the .exe extension.""")
